ed06213d819f2a41c7a80e0f422d3e7ad5eee7bf
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-07-13 17:54:18 -0400

    Working on SignupVC. Still need to figure thirdView of the SignupVC.

---
+++ Kubazar/SignupViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SignupViewController.swift
+//  Kubazar
+//
+//  Created by Alyson Vivattanapa on 7/13/16.
+//  Copyright © 2016 Jimsalabim. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    @IBOutlet weak var firstView: UIView!
+    
+    @IBOutlet weak var secondView: UIView!
+    
+    @IBOutlet weak var thirdView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        secondView.alpha = 0
+        
+        thirdView.alpha = 0
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func firstContinueButtonPressed(sender: AnyObject) {
+        
+        firstView.alpha = 0
+        secondView.alpha = 1
+    }
+    
+  
+    @IBAction func secondContinueButtonPressed(sender: AnyObject) {
+        
+        secondView.alpha = 0
+        thirdView.alpha = 1
+        
+    }
+    
+    
+
+
+}
--- Kubazar/WelcomeViewController.swift
+++ Kubazar/WelcomeViewController.swift
@@ -2,32 +2,38 @@
 //  WelcomeViewController.swift
 //  Kubazar
 //
-//  Created by Alyson Vivattanapa on 7/12/16.
+//  Created by Alyson Vivattanapa on 7/13/16.
 //  Copyright © 2016 Jimsalabim. All rights reserved.
 //
 
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var welcomeLabel: UILabel!
     
     @IBOutlet weak var kubazarLogoImageView: UIImageView!
-   
-    var timer = NSTimer()
-  
+
     @IBOutlet weak var loginButton: UIButton!
     
     @IBOutlet weak var signupButton: UIButton!
     
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+      var timer = NSTimer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         loginButton.alpha = 0
         signupButton.alpha = 0
         
+        usernameTextField.alpha = 0
+        passwordTextField.alpha = 0
+        
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(WelcomeViewController.changeWelcomeLabel), userInfo: nil, repeats: false)
-
         // Do any additional setup after loading the view.
     }
     
@@ -42,16 +48,23 @@ class WelcomeViewController: UIViewController {
         kubazarLogoImageView.alpha = 0
         loginButton.alpha = 1
         signupButton.alpha = 1
+        usernameTextField.alpha = 1
+        passwordTextField.alpha = 1
     }
-    
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+       
+    }
 
+
+    @IBAction func signupButtonPressed(sender: AnyObject) {
+        let signupVC = SignupViewController()
+        presentViewController(signupVC, animated: true, completion: nil)
+    }
     
-
 }
