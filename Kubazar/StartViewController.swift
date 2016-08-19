a82d2baa50d1d70ba9f431ebf93d42162c013690
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-08-19 09:33:55 -0400

    Continuing game flow.

--- Kubazar/StartViewController.swift
+++ Kubazar/StartViewController.swift
@@ -16,9 +16,11 @@ class StartViewController: UIViewController {
     
     @IBOutlet weak var createNewHaikuView: UIView!
     
-    @IBOutlet weak var chooseFriendsView: UIView!
-
     @IBOutlet weak var choosePictureView: UIView!
+
+    @IBOutlet weak var chooseFriendsView: UIView!
+ 
+    @IBOutlet weak var chooseFriendsLabel: UILabel!
     
     @IBOutlet weak var enterHaikuView: UIView!
     
@@ -30,7 +32,10 @@ class StartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        stepOneCreateNewHaiku()
         // Do any additional setup after loading the view.
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,15 +43,64 @@ class StartViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func stepOneCreateNewHaiku() {
+        createNewHaikuView.alpha = 1
+        chooseFriendsView.alpha = 0
+        choosePictureView.alpha = 0
+        enterHaikuView.alpha = 0
+        congratsView.alpha = 0
+    }
+    
+    func stepTwoChooseFriends() {
+        createNewHaikuView.alpha = 0
+        chooseFriendsView.alpha = 1
+        choosePictureView.alpha = 0
+        enterHaikuView.alpha = 0
+        congratsView.alpha = 0
+    }
+    
+    func stepThreeChoosePicture() {
+        createNewHaikuView.alpha = 0
+        chooseFriendsView.alpha = 0
+        choosePictureView.alpha = 1
+        enterHaikuView.alpha = 0
+        congratsView.alpha = 0
+    }
+    
+    func stepFourEnterHaiku() {
+        createNewHaikuView.alpha = 0
+        chooseFriendsView.alpha = 0
+        choosePictureView.alpha = 0
+        enterHaikuView.alpha = 1
+        congratsView.alpha = 0
+    }
+    
+    func stepFiveCongrats() {
+        createNewHaikuView.alpha = 0
+        chooseFriendsView.alpha = 0
+        choosePictureView.alpha = 0
+        enterHaikuView.alpha = 0
+        congratsView.alpha = 1
+    }
+    
+    @IBAction func startOverButtonPressed(sender: AnyObject) {
+        stepOneCreateNewHaiku()
+    }
+    
     @IBAction func twoFriendsButtonPressed(sender: AnyObject) {
         
+        chooseFriendsLabel.text = "Choose a friend!"
+        stepTwoChooseFriends()
         
     }
     
-    
-    @IBOutlet weak var threeFriendsButtonPressed: UIButton!
-    
 
+    @IBAction func threeFriendsButtonPressed(sender: AnyObject) {
+        
+        chooseFriendsLabel.text = "Choose two friends!"
+        stepTwoChooseFriends()
+        
+    }
 
 
 }
