55ff18971da5a5643d37d8e4d23ee426dcaf273f
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2016-09-07 11:10:29 +0200

    Added segmented control and current user info to infoVC.

--- Kubazar/InfoViewController.swift
+++ Kubazar/InfoViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 import Firebase
 
 class InfoViewController: UIViewController {
-
+    
+    @IBOutlet weak var myAccountView: UIView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var currentlyLoggedInLabel: UILabel!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let kubazarDarkGreen = UIColor(red: 12.0/255, green: 87.0/255, blue: 110.0/255, alpha: 1)
+        
+        segmentedControl.layer.borderColor = kubazarDarkGreen.CGColor
+        segmentedControl.tintColor = kubazarDarkGreen
 
+        self.segmentedControl.selectedSegmentIndex = 0
+
+        myAccountView.hidden = true
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +38,33 @@ class InfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        
+       ClientService.getCurrentUser({ (user) in
+        if let userEmail = user.email {
+        self.currentlyLoggedInLabel.text = "You are currently logged in as \(userEmail)"
+        } else {
+            print("error getting current user's email set to label")
+        }
+        })
+        
+        
+    }
+    
+    @IBAction func segmentedControlIndexChanged(sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            print("first item selected")
+            myAccountView.hidden = true
+        case 1:
+            print("second item selected")
+            myAccountView.hidden = false
+            
+            
+        default:
+            break
+        }
+    }
 
     @IBAction func signOutButtonPressed(sender: AnyObject) {
         try! FIRAuth.auth()!.signOut()
